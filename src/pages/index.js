4bd5206cb235c0436cd74fa47f457e23d3f7cc64
Abhishek Prakash <abhishekabby@gmail.com>
2018-08-28 11:36:58 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>I am Abhishek and I have around 8 years of experience in UI tech.</p>
+    <p>I am working as a UI lead in enVista corp.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
